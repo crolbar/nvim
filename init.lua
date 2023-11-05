914a81818d409076697f9110aa95f04418694f1a
crolbar <mni05@abv.bg>
2023-11-05 15:11:34 +0200

    nvimmm

--- init.lua
+++ init.lua
@@ -38,6 +38,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
 vim.keymap.set("n", "<leader>q", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>n", vim.cmd.noh)
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
@@ -46,6 +48,8 @@ vim.keymap.set("n", "<leader>ss", ':so<CR>:PackerSync<CR>')
 
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+vim.keymap.set('n', '<leader><Tab>', '<C-w><C-p>', { noremap = true })
 
 vim.keymap.set('n', '<M-j>', '<C-W>j', { noremap = true })
 vim.keymap.set('n', '<M-k>', '<C-W>k', { noremap = true })
