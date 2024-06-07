6ecda3e4c28e07a2eea019edefb31c71bfb81b13
crolbar <crolbar@crolbar.com>
2024-06-07 14:52:17 +0300

    harpoon more binds

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,7 +5,7 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local servers = {
     'html', 'clangd', 'rust_analyzer', 'pyright',
-    'lua_ls', 'bashls', 'cssls', 'nixd',
+    'lua_ls', 'bashls', 'cssls', 'nixd', 'tsserver'
 }
 
 for _, lsp in ipairs(servers) do
