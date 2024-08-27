2854e78cd3133806635aed1202452614754153bd
crolbar <crolbar@crolbar.com>
2024-08-27 22:52:06 +0300

    fix nvim-autopairs move right shit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,5 @@
 return {
-    { "windwp/nvim-autopairs", opts = {} },
+    { "windwp/nvim-autopairs", opts = {enable_moveright = false} },
     { "mbbill/undotree", keys = { { "<leader>u", vim.cmd.UndotreeToggle } } },
     { "ThePrimeagen/vim-be-good", cmd = "VimBeGood" }
 }
