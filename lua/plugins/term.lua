96b1802754548a2c022aa8d5a467d23ed33d001d
crolbar <crolbar@crolbar.com>
2024-08-27 23:12:48 +0300

    silet term

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -1,26 +1,24 @@
 return {
     "akinsho/toggleterm.nvim",
     keys = {
-        { '<M-K>', [[<Cmd>wincmd k<CR>]], mode = 't' },
-        { '<M-H>', [[<Cmd>wincmd h<CR>]], mode = 't' },
-        { '<M-L>', [[<Cmd>wincmd l<CR>]], mode = 't' },
-        { '<M-J>', [[<Cmd>wincmd j<CR>]], mode = 't' },
+        { '<M-K>', [[<Cmd>wincmd k<CR>]], mode = 't', {silent = true}, },
+        { '<M-H>', [[<Cmd>wincmd h<CR>]], mode = 't', {silent = true}, },
+        { '<M-L>', [[<Cmd>wincmd l<CR>]], mode = 't', {silent = true}, },
+        { '<M-J>', [[<Cmd>wincmd j<CR>]], mode = 't', {silent = true}, },
 
-        { "<M-`>", '<Cmd>1ToggleTerm <CR>', {silent = true}, mode = "t" },
-        { "<M-1>", '<Cmd>2ToggleTerm <CR>', mode = "t" },
-        { "<M-2>", '<Cmd>3ToggleTerm <CR>', mode = "t" },
+        { "<M-`>", '<Cmd>1ToggleTerm <CR>', mode = "t", {silent = true}, },
+        { "<M-1>", '<Cmd>2ToggleTerm <CR>', mode = "t", {silent = true}, },
+        { "<M-2>", '<Cmd>3ToggleTerm <CR>', mode = "t", {silent = true}, },
 
         { "<A-Esc>", '<C-\\><C-n>', mode = "t" },
 
-        --{ "<M-`>", ':1ToggleTerm size=20 direction=float <CR>', mode = "v" },
-        { "<M-`>", ':1ToggleTerm size=20 direction=float <CR>', {silent = true}, },
-        { "<M-`>", '<Esc> :1ToggleTerm size=20 direction=float <CR>', mode = { "v" , "i" }, {silent = true}, },
+        { "<M-`>", '<Esc> :1ToggleTerm size=20 direction=float <CR>', mode = { "v" , "i", "n"}, {silent = true}, },
 
-        { "<M-1>", ':2ToggleTerm size=20 direction=horizontal <CR>', mode = "n" },
-        { "<M-2>", ':3ToggleTerm size=20 direction=horizontal <CR>', mode = "n" },
+        { "<M-1>", '<Esc> :2ToggleTerm size=20 direction=horizontal <CR>', {silent = true}, },
+        { "<M-2>", '<Esc> :3ToggleTerm size=20 direction=horizontal <CR>', {silent = true}, },
 
 
-        { "<S-X>", string.format(':1TermExec cmd="%s"<CR>', "!!"), mode = "n" },
+        { "<S-X>", string.format(':1TermExec cmd="%s"<CR>', "!!"), },
     },
     opts = {},
 }
