2762c558f132ac32698c8b959b8a11e0ae0879be
crolbar <crolbar@crolbar.com>
2024-09-05 18:46:23 +0300

    nvim-surround

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,15 +1,17 @@
 return {
-    { "windwp/nvim-autopairs", opts = { enable_moveright = false } },
-    { "mbbill/undotree", keys = { { "<leader>u", vim.cmd.UndotreeToggle } } },
-    { "ThePrimeagen/vim-be-good", cmd = "VimBeGood" },
-
+    {"windwp/nvim-autopairs", opts = {enable_moveright = false}},
+    {"mbbill/undotree", keys = {{"<leader>u", vim.cmd.UndotreeToggle}}},
+    {"ThePrimeagen/vim-be-good", cmd = "VimBeGood"},
     { 'Wansmer/treesj',
         keys = {
-            { '<space>j', function () require('treesj').toggle() end },
+            {'<space>j', function () require('treesj').toggle() end},
+            {'<space>rj', function () require('treesj').toggle({split = {recursive = true}}) end},
         },
         dependencies = {'nvim-treesitter/nvim-treesitter'},
         opts = {
             use_default_keymaps = false,
+            max_join_length = 1200,
         },
     },
+    {"kylechui/nvim-surround", keys = {'ys', 'cs', 'ds', {'S', mode = 'v'}}, opts = {}},
 }
