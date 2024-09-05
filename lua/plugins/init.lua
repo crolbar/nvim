7307edaca443e0de4ae0256cfd9a6082955d800d
crolbar <crolbar@crolbar.com>
2024-09-05 18:17:25 +0300

    treesj & lazy up

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,15 @@
 return {
-    { "windwp/nvim-autopairs", opts = {enable_moveright = false} },
+    { "windwp/nvim-autopairs", opts = { enable_moveright = false } },
     { "mbbill/undotree", keys = { { "<leader>u", vim.cmd.UndotreeToggle } } },
-    { "ThePrimeagen/vim-be-good", cmd = "VimBeGood" }
+    { "ThePrimeagen/vim-be-good", cmd = "VimBeGood" },
+
+    { 'Wansmer/treesj',
+        keys = {
+            { '<space>j', function () require('treesj').toggle() end },
+        },
+        dependencies = {'nvim-treesitter/nvim-treesitter'},
+        opts = {
+            use_default_keymaps = false,
+        },
+    },
 }
