529eeb7ee61009d5b8c3ada8952f5d82b291342c
crolbar <crolbar@crolbar.com>
2024-11-09 16:13:48 +0200

    treesitter: fix php indents not working

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -15,6 +15,9 @@ return {
             highlight = {
                 enable = true,
             },
+            indent = {
+                enable = true,
+            },
         }
 
         vim.filetype.add({
