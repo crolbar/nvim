55737f2289c5cf011b591987a5058774f36d5ab5
crolbar <crolbar@crolbar.com>
2025-01-17 20:32:12 +0200

    remove autopair (whooh finally)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,4 @@
 return {
-    {"windwp/nvim-autopairs", opts = {enable_moveright = false}},
     {"mbbill/undotree", keys = {{"<leader>u", vim.cmd.UndotreeToggle}}},
     {"ThePrimeagen/vim-be-good", cmd = "VimBeGood"},
     { 'Wansmer/treesj',
